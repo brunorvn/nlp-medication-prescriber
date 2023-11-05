dbe9e7f65c05b379d9ec49253ab717a620e1428d
Bruno Reis <58519484+brunorvn@users.noreply.github.com>
2023-11-05 09:41:22 -0300

    cookiecutter-tcc

---
+++ HC/tcc_brvn/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Production analysis and prediction of UFPE university hospital',
+    author='Bruno Reis',
+    license='MIT',
+)
